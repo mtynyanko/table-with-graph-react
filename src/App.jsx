0d62b243a63ad44f99c64a79b2170cd9e55c55af
Maxim Tynyanko <data.expanged@gmail.com>
2025-02-19 18:17:49 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Container from './assets/Container'
+
+function App() {
+  return (<>
+    <Container/>
+  </>)
+}
+
+export default App
---
+++ src/assets/Container/Container.jsx
@@ -0,0 +1,8 @@
+import Item from "../Item";
+
+const Container = () => {
+  return (<>
+    <Item/>
+  </>);
+}
+export default Container
